ca2c9e5ff2400a4d9324bc3188f873dd6dbe1bc9
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-06-17 01:59:21 -0400

    dsda

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -7,7 +7,7 @@ function GoalItem({ goal }) {
   return (
     <div className='goal'>
         <div>
-            {new Date(goal.createdAt).toLocaleString}('en-us')
+            {new Date(goal.createdAt).toLocaleString('en-US')}
         </div>
         <h2>{goal.text}</h2>
         <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
